3837eada3921a5521754ec9c444f2567b57c4af1
Minhaoliu <s1252307@monmouth.edu>
2020-06-22 15:06:25 -0400

    counter

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import CounterApp from './src/counterApp'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const initialState = {
+  counter: 0
 }
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNTER':
+      return { counter: state.counter + 1 }
+    case 'DECREASE_COUNTER':
+      return { counter: state.counter - 1 }
+  }
+  return state
+}
+
+const store = createStore(reducer)
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <Provider store={store}>
+        <CounterApp />
+      </Provider>
+    );
+  }
+}
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
